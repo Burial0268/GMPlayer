28619e52ba293f5141a03af5a30cca0a3ef2cf09
Burial0268 <Burial0268@foxmail.com>
2026-02-05 17:53:12 +0800

    feat(Tauri): enhance main window setup with macOS-specific features
    - Integrate tauri_plugin_decorum for improved window management.
    - Add overlay title bar and custom traffic light insets for macOS.
    - Implement window transparency and set window level for better UI experience on macOS.
    - Refactor main function to include new setup logic for enhanced window behavior.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,14 +4,36 @@
 use app::algorithms::chunk::{chunk, ChunkedArray};
 use app::algorithms;
 use tauri::command;
+use tauri::Manager;
+use tauri_plugin_decorum::WebviewWindowExt; // adds helper methods to WebviewWindow
 
 fn main() {
   tauri::Builder::default()
+  .plugin(tauri_plugin_decorum::init())
   .invoke_handler(tauri::generate_handler![
     get_chunk,
     format_number,
     detect_desktop
     ])
+    .setup(|app| {
+			let main_window = app.get_webview_window("main").unwrap();
+			main_window.create_overlay_titlebar().unwrap();
+
+			// Some macOS-specific helpers
+			#[cfg(target_os = "macos")] {
+				// Set a custom inset to the traffic lights
+				main_window.set_traffic_lights_inset(12.0, 16.0).unwrap();
+
+				// Make window transparent without privateApi
+				main_window.make_transparent().unwrap();
+
+				// Set window level
+				// NSWindowLevel: https://developer.apple.com/documentation/appkit/nswindowlevel
+				main_window.set_window_level(25).unwrap();
+			}
+
+			Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
